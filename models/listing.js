b9777b63c94867a24964fe0e993bb49c7b991bdc
Chitra Thakur <chitrathakur83145@gmail.com>
2026-05-19 15:42:44 +0530

    Added mapbox maps and geocoding for listings

--- models/listing.js
+++ models/listing.js
@@ -11,7 +11,7 @@ const listingSchema = new Schema({
   description: String,
   image: {
     url: String,
-    filename: String
+    filename: String,
   },
   price: Number,
   location: String,
@@ -26,6 +26,17 @@ const listingSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+  geometry: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  }
 });
 
 listingSchema.post("findOneAndDelete", async (listing) => {
